42ee9703a7916bb2d1e005e6067453f8f6cbce0e
Radon Freedom <Radon.Freedom@gmail.com>
2019-02-17 13:30:55 +0800

    增加注释

--- src/sword/_57.java
+++ src/sword/_57.java
@@ -11,7 +11,8 @@ package sword;
 public class _57 {
 
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        int flag[] = new int[matrix.length];
+        int[] flag = new int[matrix.length];
+        //试着从每个位置作为起始位置查找路径，直到返回true
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (helper(matrix, rows, cols, i, j, str, 0, flag)) {
@@ -23,13 +24,20 @@ public class _57 {
     }
 
     private boolean helper(char[] matrix, int rows, int cols, int i, int j, char[] str, int k, int[] flag) {
+        //计算出i行j列对应在数组中的索引
         int index = i * cols + j;
+        //首先：i, j不能在矩阵中越界
+        //其次：矩阵该位置的字符matrix[index] == str[k]
+        //再次：不能走回头路，flag[index] == 0
         if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[index] != str[k] || flag[index] == 1) {
             return false;
         }
         if (k == str.length - 1) {
             return true;
         }
+        //走到这里说明matrix[index] == str[k]，可以进行下一个字符str[k+1]的搜索（方向为上下左右）
+        //只要有一个方向可以成功，就返回true
+        //把这个位置的flag标记为1，表明不能走回头路，只能走flag标记不是1的路径
         flag[index] = 1;
         if (helper(matrix, rows, cols, i - 1, j, str, k + 1, flag)
                 || helper(matrix, rows, cols, i + 1, j, str, k + 1, flag)
@@ -42,7 +50,7 @@ public class _57 {
     }
 
     public static void main(String[] args) {
-        
+
         char[] matrix = "abcesfcsadee".toCharArray();
         new _57().hasPath(matrix, 3, 4, "bcced".toCharArray());
     }
