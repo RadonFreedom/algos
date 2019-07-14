8db1052e8c1ccc3507d88bdc0f04ee7b5a5267b0
xufeng <xufeng@mogu.com>
2019-07-15 00:43:09 +0800

    雨水

--- src/leetcode/_42.java
+++ src/leetcode/_42.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 /**
  * @author Radon Freedom
  * created at 2019.05.29 15:37
@@ -9,17 +11,36 @@ public class _42 {
     public int trap(int[] height) {
 
         int res = 0;
-        int left, right;
-        boolean hasLeft = false;
-        for (int i = 0; i < height.length - 1; i++) {
-            if (hasLeft) {
-                if (height[i] <= height[i + 1]) {
-                    continue;
-                }
+        LinkedList<Integer> list = new LinkedList<>();
 
+        for (int i = 0; i < height.length; i++) {
+            if (list.isEmpty() || height[list.getLast()] >= height[i]) {
+                list.add(i);
+            }
+            else {
+                //不再递减，从当前高度，从右往左一个一个地观察可存水量
+                while (list.size() >= 2) {
+                    int waterPoolIndex = list.removeLast();
+                    int waterPoolDis = i - list.getLast() - 1;
+                    int waterPoolHei = Math.min(height[list.getLast()], height[i]) - height[waterPoolIndex];
+                    res += waterPoolHei * waterPoolDis;
+                    if (height[list.getLast()] > height[i]) {
+                        break;
+                    }
+                }
+                if (height[list.getLast()] <= height[i]) {
+                    list.removeLast();
+                }
+                list.add(i);
             }
         }
 
         return res;
+    }
+
+    public static void main(String[] args) {
+
+
+        new _42().trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
     }
 }
---
+++ src/leetcode/_42_2.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * @author xufeng
+ * created at 2019.07.14 23:35
+ */
+
+public class _42_2 {
+
+    /**
+     * 当前索引左侧和右侧最高值的较小值决定了本位的水量
+     */
+    public int trap2(int[] height) {
+
+        int res = 0;
+        int left = 0, right = height.length - 1;
+        int lm = 0, rm = 0;
+        while (left <= right) {
+            //计算left索引左侧的最高值
+            lm = Math.max(lm, height[left]);
+            //计算right索引右侧的最高值
+            rm = Math.max(rm, height[right]);
+            if (lm < rm) {
+                /*
+                尽管rm不一定是left的右侧最大值
+                但lm是left的左侧最大值
+                lm < rm说明lm是left的左右侧最高值的较小值
+                因此left的水高可以计算出来
+                 */
+                res += lm - height[left];
+                //计算完left索引往右走
+                left++;
+            } else {
+                res += rm - height[right];
+                right--;
+            }
+        }
+
+        return res;
+    }
+}
