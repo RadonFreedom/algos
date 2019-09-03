2a4fff28bf5b275fb4dd1b21944000c2a8b54d0d
RadonFreedom <radon.freedom@gmail.com>
2019-09-03 11:30:33 +0800

    1

--- src/leetcode/_3.java
+++ src/leetcode/_3.java
@@ -21,7 +21,7 @@ public class _3 {
             if (map.containsKey(c)) {
                 rst = Math.max(rst, hi - lo);
                 // 新的子串从重复字符的下一个字符开始
-                // 为了不用remove新lo之前的所有字符，我们在lo赋值时只取更大的值
+                // 为了不用remove掉map中新lo之前的所有字符，我们在lo赋值时只取更大的值
                 lo = Math.max(lo, map.get(c) + 1);
             }
             // 更新重复字符的索引
